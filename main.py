92e086f0b090245d04f8cdfad09dbcbcb9584821
Youssef Galal <youssefgalalnazem@gmail.com>
2022-07-20 03:45:31 +0200

    Remove everything

--- main.py
+++ main.py
@@ -1,36 +1,5 @@
-import sys
-
-from PyQt5.QtCore import QTimer
-from PyQt5.QtWidgets import QApplication, QSystemTrayIcon, QMenu, QAction
-
-from components.todo_app import ToDoApp
-from gui.window import Window
-
-
 def main():
-    q_app = QApplication(sys.argv)
-    q_app.setApplicationName("ToDo")
-    todo_app = ToDoApp()
-    q_app.setWindowIcon(todo_app.icon)
-    tray = QSystemTrayIcon(todo_app.icon, q_app)
-    todo_app.set_tray(tray)
-    window = Window(todo_app)
-    todo_app.set_window(window)
-
-    if tray.isSystemTrayAvailable():
-        q_app.setQuitOnLastWindowClosed(False)
-        tray.setToolTip(q_app.applicationName())
-        menu = QMenu()
-        show_action = QAction("Show tasks")
-        exit_action = QAction("Exit")
-        show_action.triggered.connect(window.show)
-        exit_action.triggered.connect(sys.exit)
-        menu.addAction(show_action)
-        menu.addAction(exit_action)
-        tray.setContextMenu(menu)
-        tray.show()
-    window.show()
-    q_app.exec()
+    pass
 
 
 if __name__ == '__main__':
